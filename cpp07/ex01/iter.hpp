04cf21acf48d746825d2c651d3e821fcfad9e544
takira <ca.fie.0220@gmail.com>
2023-09-12 11:32:40 +0900

    update(cpp07): update ex01

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-# include <exception>
+# include <stdexcept>
 # include <iostream>
 //Implement a function template iter that takes 3 parameters and returns nothing.
 // • The first parameter is the address of an array.
@@ -8,7 +8,7 @@
 // • The third one is a function that will be call on every element of the array.
 
 template <typename T>
-void iter(const T *addr, size_t len, void (*func)(const T&)) {
+void iter(const T *addr, const size_t len, void (*func)(const T&)) {
 	std::cout << "called: void iter(const T *addr, size_t len, void func(const T&))" << std::endl;
 	if (!addr || len == 0 || !func) {
 		throw std::invalid_argument("[Error] invalid argument");
@@ -19,7 +19,7 @@ void iter(const T *addr, size_t len, void (*func)(const T&)) {
 }
 
 template <typename T>
-void iter(T *addr, size_t len, void (*func)(T&)) {
+void iter(T *addr, const size_t len, void (*func)(T&)) {
 	std::cout << "called: void iter(T *addr, size_t len, void func(T&))" << std::endl;
 	if (!addr || len == 0 || !func) {
 		throw std::invalid_argument("[Error] invalid argument");
@@ -27,4 +27,9 @@ void iter(T *addr, size_t len, void (*func)(T&)) {
 	for (size_t i = 0; i < len; ++i) {
 		func(addr[i]);
 	}
+}
+
+template <typename T>
+void print_arr(const T &val) {
+	std::cout << val << " ";
 }
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -5,30 +5,27 @@ void twice(int &val) {
 	val *= 2;
 }
 
-template <typename T>
-void print_arr(const T &val) {
-	std::cout << val << " ";
-}
-
 int main(void) {
 	{
-		std::cout << "--------------- 1) print_arr ---------------" << std::endl;
+		std::cout << "--------------- 1) OK: const int arr[], func:print_arr ---------------" << std::endl;
 		try {
 			const int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 			size_t size = sizeof(arr) / sizeof(arr[0]);
 			iter(arr, size, print_arr);
+			std::cout << std::endl;
 		} catch (const std::exception &e) {
 			std::cout << e.what() << std::endl;
 		}
 		std::cout << std::endl;
 	}
 	{
-		std::cout << "--------------- 2) twice ---------------" << std::endl;
+		std::cout << "--------------- 2) OK: int arr[], func:twice ---------------" << std::endl;
 		try {
 			int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 			size_t size = sizeof(arr) / sizeof(arr[0]);
 			iter(arr, size, twice);
 			iter(arr, size, print_arr);
+			std::cout << std::endl;
 		} catch (const std::exception &e) {
 			std::cout << e.what() << std::endl;
 		}
